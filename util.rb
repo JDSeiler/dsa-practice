77d7bfabc5e022d63fff63fdff4c2cdd0d699bb7
Jordan Seiler <jordan.seiler@ozmoapp.com>
2024-09-13 14:39:48 -0400

    Whoops! Rename is_sorted? to sorted? and fix a rubocop cop

--- util.rb
+++ util.rb
@@ -4,12 +4,12 @@ def random_ints(size = 100, range = (-100..100))
   (1..size).map { rand(range) }
 end
 
-def is_sorted?(arr, direction = :asc)
-  raise ArgumentError.new('Direction must be one of: [:asc, :desc]') unless %i[asc desc].include?(direction)
+def sorted?(arr, direction = :asc)
+  raise ArgumentError.new, 'Direction must be one of: [:asc, :desc]' unless %i[asc desc].include?(direction)
 
   lt_eq = proc { |a, b| a <= b }
   gt_eq = proc { |a, b| a >= b }
-  comparator = lt_comparator = direction == :asc ? lt_eq : gt_eq
+  comparator = direction == :asc ? lt_eq : gt_eq
 
   arr.each_cons(2).map(&comparator).all?
 end
