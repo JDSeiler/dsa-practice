ff192efe9829fe784d548dab86327d8134bee740
Jordan Seiler <jordan.seiler@ozmoapp.com>
2024-09-25 16:26:14 -0400

    Implement quicksort based off CLRS
    IDK how much I learned doing this since I just translated their
    pseudocode

--- src/sorting/quick.rb
+++ src/sorting/quick.rb
@@ -1,4 +1,38 @@
-def quick_sort(arr)
-  puts('TODO')
-  nil
+def quicksort(arr)
+  _quicksort(arr, 0, arr.size - 1)
+  arr
+end
+
+def _quicksort(arr, s, e)
+  return unless s < e
+
+  pivot_idx = _partition(arr, s, e)
+  _quicksort(arr, s, pivot_idx - 1)
+  _quicksort(arr, pivot_idx + 1, e)
+end
+
+# s, e -> start, end
+# This is the randomized pivot algorithm from CLRS
+# It is slightly magical
+def _partition(arr, s, e)
+  random_pivot = [*s..e].sample
+  _exchange(arr, random_pivot, e)
+
+  pivot = arr[e]
+  dest = s - 1
+  for to_check in s..e - 1
+    if arr[to_check] <= pivot
+      dest += 1
+      _exchange(arr, dest, to_check)
+    end
+  end
+  _exchange(arr, dest + 1, e)
+  # Location of the pivot
+  dest + 1
+end
+
+def _exchange(arr, i, j)
+  tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
 end
